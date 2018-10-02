33d343fb95f4788815557dc5caec4a84c9ff4ce7
marycodes2 <mbeliveau92@gmail.com>
2018-10-02 17:00:50 -0400

    Added access_api file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 require 'bundler'
+require 'nokogiri'
+require 'watir'
+require 'pry'
+require 'sinatra/activerecord'
+require_relative "../app/models/period.rb"
+require_relative "../app/models/region.rb"
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+require_relative "../lib/access_api.rb"
